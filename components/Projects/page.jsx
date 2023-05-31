c6958da0014f3b8489a133e3648649c5667ba30a
senhai-kun <senpaikouhai04@gmail.com>
2023-05-31 22:00:35 +0800

    fixed errors

--- components/Projects/page.jsx
+++ components/Projects/page.jsx
@@ -8,15 +8,17 @@ const Project = () => {
         <>
             <h1 className="text-4xl font-bold">Projects</h1>
             <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 pt-10">
-                {data.map((data) => (
-                    <Card
-                        image={data.image}
-                        title={data.title}
-                        description={data.description}
-                        tags={data.tags}
-                        preview={data.preview}
-                        github={data.github}
-                    />
+                {data.map((data, index) => (
+                    <React.Fragment key={index}>
+                        <Card
+                            image={data.image}
+                            title={data.title}
+                            description={data.description}
+                            tags={data.tags}
+                            preview={data.preview}
+                            github={data.github}
+                        />
+                    </React.Fragment>
                 ))}
             </section>
         </>
--- components/Skills/page.jsx
+++ components/Skills/page.jsx
@@ -7,8 +7,8 @@ const Skills = () => {
             <h1 id="technologies" className="text-4xl font-bold" >Skills | Technologies</h1>
 
             <div className="flex flex-wrap justify-evenly gap-10 grow mt-10" >
-                {data.map( skill => (
-                    <div  className="bg-gray-800 rounded-lg p-2 md:p-4 place-items-center grid grid-cols-1 place-content-between">
+                {data.map( (skill, index) => (
+                    <div key={index} className="bg-gray-800 rounded-lg p-2 md:p-4 place-items-center grid grid-cols-1 place-content-between">
                         <Image className="rounded-md w-1/2 h-auto " src={skill.image} alt={skill.title} width={100} height={100}  />
                         <h3 className="text-lg font-semibold mt-1" >{skill.title}</h3>
                     </div>
